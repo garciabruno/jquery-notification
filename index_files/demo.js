e85db0dea952d6d94aa27a457807fd1fdbe3a521
Trent <garciabruno@live.com.ar>
2015-01-12 18:10:06 -0300

    Removed demo page from master branch

--- index_files/demo.js
+++
@@ -1,6 +0,0 @@
-$('.demos li > a').on('click', function() {
-	var container = $(this).next('pre'),
-		source = container.find('code').html().replace('&lt;', '<').replace('&gt;', '>');
-	container.show();
-	eval(source);
-});
